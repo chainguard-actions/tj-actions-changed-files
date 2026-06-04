bafde266eb8f4164e6c100fb43b37a2e99bc6f17
actionchain <actionchain@chainguard.dev>
2026-06-04 11:34:56 +0000

    Update tj-actions/changed-files@v47.0.6

---
+++ src/constant.ts
@@ -0,0 +1,31 @@
+import {Inputs} from './inputs'
+
+export const DEFAULT_VALUES_OF_UNSUPPORTED_API_INPUTS: Partial<Inputs> = {
+  sha: '',
+  baseSha: '',
+  since: '',
+  until: '',
+  path: '.',
+  quotepath: true,
+  diffRelative: true,
+  sinceLastRemoteCommit: false,
+  recoverDeletedFiles: false,
+  recoverDeletedFilesToDestination: '',
+  recoverFiles: '',
+  recoverFilesSeparator: '\n',
+  recoverFilesIgnore: '',
+  recoverFilesIgnoreSeparator: '\n',
+  includeAllOldNewRenamedFiles: false,
+  oldNewSeparator: ',',
+  oldNewFilesSeparator: ' ',
+  skipInitialFetch: false,
+  fetchAdditionalSubmoduleHistory: false,
+  dirNamesDeletedFilesIncludeOnlyDeletedDirs: false,
+  excludeSubmodules: false,
+  excludeSymlinks: false,
+  skipSameSha: false,
+  fetchMissingHistoryMaxRetries: 20,
+  usePosixPathSeparator: false,
+  tagsPattern: '*',
+  tagsIgnorePattern: ''
+}
